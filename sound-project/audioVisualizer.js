cf9ef43ae8da81df5da0f8c2ce78e3dd240f0fae
Justin <justin@jjohnson.io>
2024-11-20 13:03:15 -0500

    rotating circle mapping amp

--- sound-project/audioVisualizer.js
+++ sound-project/audioVisualizer.js
@@ -5,9 +5,10 @@ let song,
 
 function setup() {
   createCanvas(400, 400);
+  background(0);
   song = loadSound("audio/gman.mp3", loaded);
   amp = new p5.Amplitude();
-  background(0);
+  angleMode(DEGREES);
 }
 
 function loaded() {
@@ -21,14 +22,18 @@ function draw() {
   volHistory.push(vol);
   stroke(255);
   noFill();
+
+  translate(width / 2, height / 2);
   beginShape();
-  for (var i = 0; i < volHistory.length; i++) {
-    let y = map(volHistory[i], 0, 1, height, 0);
-    vertex(i, y);
+  for (let i = 0; i < 360; i++) {
+    let r = map(volHistory[i], 0, 1, 10, 500);
+    let x = r * cos(i);
+    let y = r * sin(i);
+    vertex(x, y);
   }
   endShape();
 
-  if (volHistory.length > width) {
+  if (volHistory.length > 360) {
     volHistory.splice(0, 1);
   }
 }
