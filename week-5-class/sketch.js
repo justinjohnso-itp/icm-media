9cfcf7d5df7ed119061e3854afc3bfb22876b9fa
Justin <justin@jjohnson.io>
2024-11-20 19:48:11 -0500

    add week 5 class

---
+++ week-5-class/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(0);
+  fill("green");
+  stroke("white");
+  strokeWeight(5);
+  // textSize(48);
+  // textSize(12 + map(mouseX, 0, width, 0, 144));
+  // text("Wheeeeeeee", 50, 200);
+  textSize(72);
+  text("yee haw", mouseX, mouseY);
+  // fill(200);
+  // noStroke();
+  // ellipse(50, 300, 15, 15);
+}
